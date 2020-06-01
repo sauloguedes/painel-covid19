ffcf34f51202995550565c207f2dcc23245b8f0a
Saulo <saulo.guedes.jr@gmail.com>
2020-05-31 21:24:27 -0300

    Creating deaths component

--- src/app/core/deaths/deaths.component.ts
+++ src/app/core/deaths/deaths.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { obitosAcumulados } from './data/obitosAcumulados';
+import { obitosPorDia } from './data/obitosPorDia';
 
 @Component({
   selector: 'app-deaths',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeathsComponent implements OnInit {
 
+  public data = [];
+
+  public view: any[] = [];
+
+  colorScheme = {
+    domain: ['#5AA454']
+  };
+
+  private days = obitosAcumulados.length
+
+  public mediaObitosDia: number = 27878 / this.days
+
   constructor() { }
 
   ngOnInit(): void {
+
+    this.setObitosAcumulado()
+
+  }
+
+  setObitosAcumulado(): void {
+    this.data = obitosAcumulados
+  }
+
+  setObitosPordia(): void {
+    this.data = obitosPorDia
   }
 
 }
